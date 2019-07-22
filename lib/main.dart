81ca7b7d7d2a1ebae009586d1c62f2b50d5023e3
Ginette <ginettv@thoughtworks.com>
2019-07-22 12:04:25 +0530

    Ginette - updating logos in the app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wastexchange_mobile/screens/waste_exchange_map_screen.dart';
+import 'package:wastexchange_mobile/screens/HomeScreen.dart';
 
-void main() => runApp(WasteExchangeApp());
-
-class WasteExchangeApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Waste Exchange",
-      home: WasteExchangeMapScreen(),
-    );
-  }
-}
+void main() => runApp(HomeScreen());
---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:wastexchange_mobile/widgets/HomeAppBar.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Waste Exchange',
+      home: Scaffold(
+        appBar: HomeAppBar(),
+        body: Center(
+          child: Text('Welcome'),
+        ),
+      ),
+    );
+  }
+}
