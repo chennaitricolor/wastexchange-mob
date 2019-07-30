164429b9d51b56fb4b5a36ebf54cce1641b58c6b
chandru612 <chandru.androiddeveloper@gmail.com>
2019-07-30 22:20:37 +0530

    [Chandru] Draws polyline between given points

---
+++ lib/commons/constants.dart
@@ -0,0 +1,7 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Constants {
+  static const LatLng LAT_LNG_THOUGHTWORKS = LatLng(12.9838, 80.2459);
+  static const LatLng LAT_LNG_AIRPORT = LatLng(12.9941, 80.1709);
+  static const LatLng LAT_LNG_CENTRAL = LatLng(13.0823, 80.2755);
+}
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:wastexchange_mobile/commons/constants.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -10,11 +11,12 @@ class _MapState extends State<MapScreen> {
   GoogleMapController _mapController;
   MapType _type = MapType.normal;
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  Set<Polyline> polylines = { Polyline(width: 5, color: Colors.green, polylineId: PolylineId("1"), points: [Constants.LAT_LNG_THOUGHTWORKS, Constants.LAT_LNG_AIRPORT, Constants.LAT_LNG_CENTRAL]) };
 
   @override
   void initState() {
     super.initState();
-    addMarker();
+    initMarker();
   }
 
   @override
@@ -23,8 +25,8 @@ class _MapState extends State<MapScreen> {
   }
 
   static final _options = CameraPosition(
-    target: LatLng(12.9838, 80.2459),
-    zoom: 15,
+    target: Constants.LAT_LNG_AIRPORT,
+    zoom: 12,
   );
 
   void onMapCreated(GoogleMapController controller) {
@@ -37,6 +39,7 @@ class _MapState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
+          polylines: polylines,
           initialCameraPosition: _options,
           onMapCreated: onMapCreated,
           mapType: _type,
@@ -46,18 +49,22 @@ class _MapState extends State<MapScreen> {
     );
   }
 
-  void addMarker() {
-    final Marker marker = Marker(
-      markerId: MarkerId("1"),
-      position: LatLng(12.9838, 80.2459),
-      infoWindow: InfoWindow(title: "ThoughtWorks Technologies India Pvt Ltd", snippet: 'Taramani'),
+  void initMarker() {
+    addMarker("1", "ThoughtWorks Technologies India Pvt Ltd", 'Taramani', Constants.LAT_LNG_THOUGHTWORKS);
+    addMarker("2", "Kamaraj Domestic Terminal", 'Airport', Constants.LAT_LNG_AIRPORT);
+    addMarker("3", "Chennai Central", 'Railway Station', Constants.LAT_LNG_CENTRAL);
+  }
+
+  void addMarker(String markerId, String title, String snippet, LatLng latLng) {
+    markers[MarkerId(markerId)] = Marker(
+      markerId: MarkerId(markerId),
+      position: latLng,
+      infoWindow: InfoWindow(
+          title: title, snippet: snippet),
       onTap: () {
-        _onMarkerTapped(MarkerId("1"));
+        _onMarkerTapped(MarkerId(markerId));
       },
     );
-    setState(() {
-      markers[MarkerId("1")] = marker;
-    });
   }
 
   void _onMarkerTapped(MarkerId markerId) {
