eefd7750cab3671fdc4b8a78bae7f6cc2ec85c29
chandru612 <chandru.androiddeveloper@gmail.com>
2019-07-30 20:35:32 +0530

    [Chandru] Fixes app bar title out of bounds issue

--- lib/widgets/home_app_bar.dart
+++ lib/widgets/home_app_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeAppBar extends AppBar {
-  final _formKey = new GlobalKey<FormState>();
   HomeAppBar({Key key})
       : super(
             key: key,
@@ -12,17 +11,16 @@ class HomeAppBar extends AppBar {
               children: [
                 Image.asset(
                   'assets/images/ministry-logo.png',
-                  fit: BoxFit.contain,
                   height: 32,
+                  fit: BoxFit.contain,
                 ),
-                Container(
-                    padding: const EdgeInsets.all(8.0),
+                Expanded(
                     child: Text('India Waste Exchange',
-                        style: TextStyle(color: Colors.black))),
+                        style: TextStyle(fontSize: 14, color: Colors.black))),
                 Image.asset(
                   'assets/images/smart-city-logo.png',
-                  fit: BoxFit.contain,
                   height: 32,
+                  fit: BoxFit.contain,
                 ),
               ],
             ));
