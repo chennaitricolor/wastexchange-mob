ad451dc113bccdbd48bc204a0ed22e5c51167e27
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2019-08-01 01:06:33 +0530

    [Sayeed] Fix test names

--- test/util/field_validator_test.dart
+++ test/util/field_validator_test.dart
@@ -2,17 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wastexchange_mobile/util/field_validator.dart';  
 
 void main() {
-  test('Empty Password Test', () {
+  test('empty password', () {
     var result = FieldValidator.validatePassword('');
     expect(result, 'Password cannot be empty', );
   });
 
-  test('Password < 7 Characters Tests', () {
+  test('password < 7 chars', () {
     var result = FieldValidator.validatePassword('abcdef');
     expect(result, 'Password must be more than 6 characters');
   });
 
-  test('Password >= 7 Characters Tests', () {
+  test('password >= 7 chars', () {
     var result = FieldValidator.validatePassword('abcdefg');
     expect(result, null);
   });
