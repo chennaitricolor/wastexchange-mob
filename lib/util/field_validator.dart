657f7a188e97c92ebf49697627ece709cb2debc3
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2019-08-01 11:16:51 +0530

    [Sayeed] Change password validation

--- lib/util/field_validator.dart
+++ lib/util/field_validator.dart
@@ -2,13 +2,13 @@ class FieldValidator {
 
   static String validatePassword(String value) {
     if (value.isEmpty) return 'Password cannot be empty';
-    if (value.length < 7) return 'Password must be more than 6 characters';
+    if (value.length < 5) return 'Password must be more than 5 characters';
     return null;
   }
 
   static String validateEmailAddress(String value) {
     if (value.isEmpty) return 'Email cannot be empty';
-    if (value.length < 7) return 'Email must be more than 6 characters';
+    if (value.length < 5) return 'Email must be more than 5 characters';
     return null;
   }
 }
