317a1f5292bff0164a02e4419cce2ee699b4e53f
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2019-08-02 02:22:54 +0530

    [Sayeed] Tests clean up for api_provider

--- test/resources/api_provider_test.dart
+++ test/resources/api_provider_test.dart
@@ -4,16 +4,13 @@ import 'package:http/http.dart' as http;
 import 'package:wastexchange_mobile/models/api_response_exception.dart';
 import 'package:wastexchange_mobile/models/login_response.dart';
 import 'package:wastexchange_mobile/resources/api_provider.dart';
-import 'dart:io';
 
 class MockClient extends Mock implements http.Client {}
 
 void main() {
   test('non 200 status code', () {
     final client = MockClient();
-    when(client.post(
-            argThat(
-                startsWith('http://data.indiawasteexchange.com/users/login')),
+    when(client.post('http://data.indiawasteexchange.com/users/login',
             body: anyNamed('body')))
         .thenAnswer(
             (_) async => http.Response('{"auth":true,"token":"token"}', 201));
@@ -26,9 +23,7 @@ void main() {
 
   test('200 status code', () {
     final client = MockClient();
-    when(client.post(
-            argThat(
-                startsWith('http://data.indiawasteexchange.com/users/login')),
+    when(client.post('http://data.indiawasteexchange.com/users/login',
             body: anyNamed('body')))
         .thenAnswer(
             (_) async => http.Response('{"auth":true,"token":"token"}', 200));
@@ -37,4 +32,22 @@ void main() {
 
     expect(provider.login('a', 'b'), completion(TypeMatcher<LoginResponse>()));
   });
+
+/* //TODO: Fix it.
+  test('args', () {
+    final client = MockClient();
+    when(client.post('http://data.indiawasteexchange.com/users/login',
+            body: anyNamed('body')))
+        .thenAnswer(
+            (_) async => http.Response('{"auth":true,"token":"token"}', 200));
+
+    ApiProvider provider = ApiProvider(client);
+   
+    expect(
+        provider.login('a', 'b'),
+        completion(verify(client.post(
+            'http://data.indiawasteexchange.com/users/login',
+            body: {'loginId': 'a', 'passowrd': 'b'}))));
+  });
+  */
 }
