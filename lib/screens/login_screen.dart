7a74ff79bbb3311efcf2c9d1dc4ec63804717b50
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2019-08-01 10:22:47 +0530

    [Sayeed] Add login api call to Login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,30 +8,25 @@ import 'package:wastexchange_mobile/util/field_validator.dart';
 import 'package:wastexchange_mobile/widgets/home_app_bar.dart';
 
 class LoginScreen extends StatelessWidget {
-
-  FieldValidator validator;
-
-  LoginScreen() {
-    validator = FieldValidator();
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: AuthenticationView(
-          field1Validator: (value) => validator.validateEmailAddress(value),
-          field2Validator: (value) => validator.validatePassword(value),
+          field1Validator: (value) =>
+              FieldValidator.validateEmailAddress(value),
+          field2Validator: (value) => FieldValidator.validatePassword(value),
           buttonText: Constants.LOGIN_BUTTON,
           headerLayout: HomeAppBar(),
           screenType: ScreenType.LOGIN,
           fieldTypes: [FieldType.EMAIL, FieldType.PASSWORD],
           onValidation: (bool isValidationSuccess, List<String> fieldValues) {
             if (isValidationSuccess) {
-              debugPrint("email and password fields are " +
-                  fieldValues[0] +
-                  fieldValues[1]);
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => MapScreen()));
+              ApiProvider().login(fieldValues[0], fieldValues[1]).then((value) {
+                if (value.auth) {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MapScreen()));
+                }
+              });
             }
           }),
     );
--- lib/util/field_validator.dart
+++ lib/util/field_validator.dart
@@ -1,12 +1,12 @@
 class FieldValidator {
 
-  String validatePassword(String value) {
+  static String validatePassword(String value) {
     if (value.isEmpty) return 'Password cannot be empty';
     if (value.length < 7) return 'Password must be more than 6 characters';
     return null;
   }
 
-  String validateEmailAddress(String value) {
+  static String validateEmailAddress(String value) {
     if (value.isEmpty) return 'Email cannot be empty';
     if (value.length < 7) return 'Email must be more than 6 characters';
     return null;
