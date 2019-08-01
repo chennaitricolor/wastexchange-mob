55dabcc54e8eb05e4a3121a8ce10097939679cbc
chandru612 <chandru.androiddeveloper@gmail.com>
2019-08-01 08:35:06 +0530

    [Chandru] Adds validators for email and password fields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,21 +3,36 @@ import 'package:flutter/material.dart';
 import 'package:wastexchange_mobile/screens/map_screen.dart';
 import 'package:authentication_view/authentication_view.dart';
 import 'package:wastexchange_mobile/util/constants.dart';
+import 'package:wastexchange_mobile/util/field_validator.dart';
 import 'package:wastexchange_mobile/widgets/home_app_bar.dart';
 
 class LoginScreen extends StatelessWidget {
+
+  FieldValidator validator;
+
+  LoginScreen() {
+    validator = FieldValidator();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: AuthenticationView(buttonText: Constants.LOGIN_BUTTON, headerLayout: HomeAppBar(),screenType: ScreenType.LOGIN, fieldTypes: [FieldType.EMAIL, FieldType.PASSWORD], onValidation: (bool isValidationSuccess, List<String> fieldValues) {
-        if (isValidationSuccess) {
-          debugPrint("email and password fields are " + fieldValues[0] + fieldValues[1]);
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MapScreen()));
-        }
-      }),
+      body: AuthenticationView(
+          field1Validator: (value) => validator.validateEmailAddress(value),
+          field2Validator: (value) => validator.validatePassword(value),
+          buttonText: Constants.LOGIN_BUTTON,
+          headerLayout: HomeAppBar(),
+          screenType: ScreenType.LOGIN,
+          fieldTypes: [FieldType.EMAIL, FieldType.PASSWORD],
+          onValidation: (bool isValidationSuccess, List<String> fieldValues) {
+            if (isValidationSuccess) {
+              debugPrint("email and password fields are " +
+                  fieldValues[0] +
+                  fieldValues[1]);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MapScreen()));
+            }
+          }),
     );
   }
 }
--- lib/util/field_validator.dart
+++ lib/util/field_validator.dart
@@ -1,9 +1,14 @@
 class FieldValidator {
 
-  static String validatePassword(String value) {
+  String validatePassword(String value) {
     if (value.isEmpty) return 'Password cannot be empty';
     if (value.length < 7) return 'Password must be more than 6 characters';
     return null;
   }
 
+  String validateEmailAddress(String value) {
+    if (value.isEmpty) return 'Email cannot be empty';
+    if (value.length < 7) return 'Email must be more than 6 characters';
+    return null;
+  }
 }
