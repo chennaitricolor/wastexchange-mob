007d9b95f892e09bba7a83bee6f91c183c8651df
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2019-08-02 01:01:11 +0530

    [Sayeed] Alignment and make tests match custom expection

--- test/models/login_api_response_test.dart
+++ test/models/login_api_response_test.dart
@@ -1,25 +1,26 @@
-  import 'package:flutter_test/flutter_test.dart';
-  import 'package:wastexchange_mobile/models/login_response.dart';
+import 'package:wastexchange_mobile/models/api_response_exception.dart';
+import 'package:wastexchange_mobile/models/login_response.dart';
+import 'package:test/test.dart';
 
-  void main() {
+void main() {
+  test('auth key missing', () {
+    var map = Map<String, dynamic>();
+    map['token'] = 'Blah';
+    expect(() => LoginResponse.fromJson(map),
+        throwsA(const TypeMatcher<ApiResponseException>())); 
+  });
 
-    test('auth key missing', () {
-      var map = Map<String, dynamic>();
-      map['token'] = 'Blah';
-      expect(() => LoginResponse.fromJson(map), throwsException); //TODO: Try to get throwsA(TypeMatcher<ApiResponseException>() to work
-    });
+  test('token key missing', () {
+    var map = Map<String, dynamic>();
+    map['auth'] = true;
+    expect(() => LoginResponse.fromJson(map),
+        throwsA(const TypeMatcher<ApiResponseException>())); 
+  });
 
-      test('token key missing', () {
-      var map = Map<String, dynamic>();
-      map['auth'] = true;
-      expect(() => LoginResponse.fromJson(map), throwsException); //TODO: Try to get throwsA(TypeMatcher<ApiResponseException>() to work
-    });
-
-      test('valid', () {
-      var map = Map<String, dynamic>();
-      map['auth'] = true;
-      map['token'] = 'Blah';
-      expect(() => LoginResponse.fromJson(map), returnsNormally);
-    });
-
-  }
+  test('valid', () {
+    var map = Map<String, dynamic>();
+    map['auth'] = true;
+    map['token'] = 'Blah';
+    expect(LoginResponse.fromJson(map), TypeMatcher<LoginResponse>());
+  });
+}
