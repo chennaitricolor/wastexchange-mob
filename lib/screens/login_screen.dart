5375c538f42f8ca166bece308f004d314e1b22a6
chandru612 <chandru.androiddeveloper@gmail.com>
2019-08-07 09:07:27 +0530

    [Chandru] Bumps up the authentication view

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,22 +1,23 @@
+import 'package:authentication_view/authentication_view.dart';
 import 'package:authentication_view/field_style.dart';
 import 'package:authentication_view/field_type.dart';
-import 'package:authentication_view/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:wastexchange_mobile/models/login_data.dart';
+import 'package:wastexchange_mobile/resources/user_client.dart';
 import 'package:wastexchange_mobile/screens/forgot_password_screen.dart';
 import 'package:wastexchange_mobile/screens/map_screen.dart';
 import 'package:wastexchange_mobile/screens/registration_screen.dart';
 import 'package:wastexchange_mobile/util/app_colors.dart';
 import 'package:wastexchange_mobile/util/constants.dart';
 import 'package:wastexchange_mobile/util/field_validator.dart';
-import 'package:wastexchange_mobile/resources/user_client.dart';
 import 'package:wastexchange_mobile/widgets/home_app_bar.dart';
+
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LoginView(
+      body: AuthenticationView(
           placeHolderBelowButton: MaterialButton(
               onPressed: () {
                 Navigator.push(
@@ -42,20 +43,26 @@ class LoginScreen extends StatelessWidget {
               },
               child: Text(Constants.LOGIN_FORGOT_PASSWORD,
                   style: TextStyle(color: AppColors.text_grey))),
-          fieldStyle: FieldStyle.value(0, 8, 24, const EdgeInsets.all(36),
+          fieldStyle: FieldStyle.value(16, 8, 24, 36,
               AppColors.underline, AppColors.green, AppColors.text_grey),
-          field1Validator: (value) =>
-              FieldValidator.validateEmailAddress(value),
-          field2Validator: (value) => FieldValidator.validatePassword(value),
+          fieldValidator: (value, index) {
+            switch(index){
+              case 0:
+                return FieldValidator.validateEmailAddress(value);
+              case 1:
+                return FieldValidator.validatePassword(value);
+              default:
+                return null;
+            }
+          },
           headerLayout: HomeAppBar(),
           fieldTypes: [FieldType.EMAIL, FieldType.PASSWORD],
-          onValidation: (bool isValidationSuccess, String emailValue,
-              String passwordValue) {
+          onValidation: (isValidationSuccess, textEditingControllers) {
             if (!isValidationSuccess) {
               return;
             }
             UserClient()
-                .login(LoginData(loginId: emailValue, password: passwordValue))
+                .login(LoginData(loginId: textEditingControllers[0].text, password: textEditingControllers[1].text))
                 .then((response) {
               if (response.success) {
                 Navigator.push(context,
