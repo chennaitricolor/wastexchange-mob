2714dd4ec99fa2d7b0d2db1e551cc30d20f07901
Sayeed <sayeedhussain19@gmail.com>
2019-08-12 20:49:00 +0530

    [Sayeed] Get logger level from config file

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -1,6 +1,21 @@
 import 'package:logger/logger.dart';
 import 'package:wastexchange_mobile/util/log_printer.dart';
 
+Level getLoggerLevel(String level) {
+  switch (level) {
+    case 'debug':
+      return Level.debug;
+    case 'info':
+      return Level.info;
+    case 'warning':
+      return Level.warning;
+    case 'error':
+      return Level.error;
+    default:
+      return Level.error;
+  }
+}
+
 Logger getLogger(String className) {
   return Logger(printer: SimpleLogPrinter(className));
 }
