8003e521c6829892a7a118a4c36f6b991c0ba627
chandru612 <chandru.androiddeveloper@gmail.com>
2019-08-31 14:31:58 +0530

    [Chandru] Reduces the card view elevation

--- lib/widgets/card_view.dart
+++ lib/widgets/card_view.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 
 class CardView extends Card {
 
-  CardView({@required Widget child}) : super(elevation: 2, child: child, margin: const EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4));
+  CardView({@required Widget child}) : super(elevation: 1, child: child, margin: const EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4));
 }
