e290c214132b5b721b7b1a75e6887fcb2caf5611
Sayeed M Hussain <sayeedhussain19@gmail.com>
2019-09-15 13:03:16 +0530

    [Sayeed] Simplify Drawer logic

--- lib/widgets/views/DrawerView.dart
+++ lib/widgets/views/DrawerView.dart
@@ -5,6 +5,7 @@ import 'package:wastexchange_mobile/routes/router.dart';
 import 'package:wastexchange_mobile/screens/my_bids_screen.dart';
 import 'package:wastexchange_mobile/utils/app_colors.dart';
 import 'package:wastexchange_mobile/utils/app_theme.dart';
+import 'package:wastexchange_mobile/utils/global_utils.dart';
 import 'package:wastexchange_mobile/widgets/views/drawer_item_view.dart';
 
 class DrawerView extends StatelessWidget {
@@ -23,61 +24,67 @@ class DrawerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: _thisUser != null ? <Widget>[
-        UserAccountsDrawerHeader(
-          decoration: BoxDecoration(color: AppColors.green),
-          accountName:
-          Text(_thisUser?.name ?? 'Guest', style: AppTheme.titleWhite),
-          accountEmail:
-          Text(_thisUser?.emailId ?? '', style: AppTheme.subtitleWhite),
-          currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.yellow,
-              child: Text(_thisUser?.name??"G".substring(0, 1).toUpperCase() ?? 'G',
-                  style: AppTheme.title)),
-        ),
-        DrawerItemView(
-          iconData: Icons.home,
-          text: 'Home',
-          onItemPressed: () {
-            closeDrawer(context);
-          },
-        ),
-        DrawerItemView(
-          iconData: Icons.casino,
-          text: 'My Bids',
-          onItemPressed: () {
-            Router.popAndPushNamed(context, MyBidsScreen.routeName);
-          },
-        ),
-        Divider(),
-        DrawerItemView(
-          iconData: Icons.power_settings_new,
-          text: 'Logout',
-          onItemPressed: () {
-            closeDrawer(context);
-          },
-        ),
-      ]:<Widget>[
-        UserAccountsDrawerHeader(
-          decoration: BoxDecoration(color: AppColors.green),
-          accountName: Text('Guest', style: AppTheme.titleWhite),
-          accountEmail:
-          Text(_thisUser?.emailId ?? '', style: AppTheme.subtitleWhite),
-          currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.yellow,
-              child: Text(_thisUser?.name??"G".substring(0, 1).toUpperCase() ?? 'G',
-                  style: AppTheme.title)),
-        ),
-        DrawerItemView(
-          iconData: Icons.home,
-          text: 'Home',
-          onItemPressed: () {
-            closeDrawer(context);
-          },
-        ),
-        Divider(),
-      ],
-    );
+        children: isNull(_thisUser)
+            ? signedOutWidgets(context)
+            : signedInWidgets(context));
+  }
+
+  List<Widget> signedOutWidgets(BuildContext context) {
+    return [
+      UserAccountsDrawerHeader(
+        decoration: BoxDecoration(color: AppColors.green),
+        accountName: Text('Hello, Guest', style: AppTheme.titleWhite),
+        accountEmail: Text('', style: AppTheme.subtitleWhite),
+        currentAccountPicture: CircleAvatar(
+            backgroundColor: Colors.yellow,
+            child: Text('G', style: AppTheme.title)),
+      ),
+      DrawerItemView(
+        iconData: Icons.home,
+        text: 'Home',
+        onItemPressed: () {
+          closeDrawer(context);
+        },
+      ),
+      Divider(),
+    ];
+  }
+
+  List<Widget> signedInWidgets(BuildContext context) {
+    return [
+      UserAccountsDrawerHeader(
+        decoration: BoxDecoration(color: AppColors.green),
+        accountName:
+            Text('Hello, ${_thisUser.name}', style: AppTheme.titleWhite),
+        accountEmail: Text(_thisUser.emailId, style: AppTheme.subtitleWhite),
+        currentAccountPicture: CircleAvatar(
+            backgroundColor: Colors.yellow,
+            child: Text(_thisUser.name.substring(0, 1).toUpperCase(),
+                style: AppTheme.title)),
+      ),
+      DrawerItemView(
+        iconData: Icons.home,
+        text: 'Home',
+        onItemPressed: () {
+          closeDrawer(context);
+        },
+      ),
+      DrawerItemView(
+        iconData: Icons.casino,
+        text: 'My Bids',
+        onItemPressed: () {
+          Router.popAndPushNamed(context, MyBidsScreen.routeName);
+        },
+      ),
+      Divider(),
+      DrawerItemView(
+        iconData: Icons.power_settings_new,
+        text: 'Logout',
+        onItemPressed: () {
+          closeDrawer(context);
+        },
+      ),
+    ];
   }
 
   void closeDrawer(BuildContext context) {
