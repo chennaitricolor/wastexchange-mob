7c776ef8080ccb72e558484aae01f268ba4665e6
chandru612 <chandru.androiddeveloper@gmail.com>
2019-09-19 12:23:39 +0530

    [Chandru] Sets id for forgot password screen

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -18,7 +18,7 @@ class ForgotPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final String localeEmailId = 
         AppLocalizations.of(context).translate(LocaleConstants.EMAIL_FIELD);
-    final FieldType _email = FieldType.value(localeEmailId, 50, TextInputType.emailAddress, false);
+    final FieldType _email = FieldType.value(Constants.ID_EMAIL, localeEmailId, 50, TextInputType.emailAddress, false);
     return Scaffold(
       body: AuthenticationView(
           messageLayout: Container(
