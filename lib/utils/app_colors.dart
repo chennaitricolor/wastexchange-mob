812b742529b33ce9c1d980efb35b2c0889a54f2c
Sayeed M Hussain <sayeedhussain19@gmail.com>
2019-09-25 22:07:03 +0530

    [Sayeed] Change colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -19,8 +19,8 @@ class AppColors {
 
   //App specific colors
   static const Color disabled = Color.fromARGB(255, 222, 222, 222);
-  static const Color green = Color.fromARGB(255, 64, 182, 126);
-  //static const Color green = Color.fromARGB(255, 3, 192, 74);
+  static const Color green = Color.fromARGB(255, 3, 192, 74);
+  static const Color avatar_bg = Color.fromARGB(255, 255, 236, 204);
   static const Color text_grey = Color.fromARGB(255, 174, 172, 172);
   static const Color underline = Color.fromARGB(255, 226, 226, 226);
   static const Color text_black = Color.fromARGB(255, 38, 36, 48);
