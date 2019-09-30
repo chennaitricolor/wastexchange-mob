9e70069133c914d009fa789ff19feb2b65a2ed5b
chandru612 <chandru.androiddeveloper@gmail.com>
2019-09-30 18:19:13 +0530

    [Chandru] Adds bottom action view container for buyer confirmation screen

--- lib/widgets/order_form_total.dart
+++ lib/widgets/order_form_total.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wastexchange_mobile/utils/app_theme.dart';
 import 'package:wastexchange_mobile/utils/constants.dart';
+import 'package:wastexchange_mobile/widgets/views/bottom_action_view_container.dart';
 import 'package:wastexchange_mobile/widgets/views/button_view_icon_compact.dart';
 
 class OrderFormTotal extends StatelessWidget {
@@ -12,36 +13,31 @@ class OrderFormTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      height: 90,
-      padding: const EdgeInsets.all(16),
-      child: Row(children: <Widget>[
-        Expanded(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      style: AppTheme.title,
-                      children: <TextSpan>[
-                        const TextSpan(text: 'Total: '),
-                        TextSpan(
-                            text:
-                                '${Constants.INR_UNICODE}${total.toStringAsFixed(2)}',
-                            style: AppTheme.body1),
-                      ],
-                    ),
+    return BottomActionViewContainer(children: <Widget>[
+      Expanded(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    style: AppTheme.title,
+                    children: <TextSpan>[
+                      const TextSpan(text: 'Total: '),
+                      TextSpan(
+                          text:
+                          '${Constants.INR_UNICODE}${total.toStringAsFixed(2)}',
+                          style: AppTheme.body1),
+                    ],
                   ),
-                  Text(
-                    '$itemsCount item' + (itemsCount == 1 ? '' : 's'),
-                    style: AppTheme.body2,
-                  )
-                ]),
-            flex: 1),
-        ButtonViewIconCompact(text: 'Confirm', onPressed: onPressed),
-      ]),
-    );
+                ),
+                Text(
+                  '$itemsCount item' + (itemsCount == 1 ? '' : 's'),
+                  style: AppTheme.body2,
+                )
+              ]),
+          flex: 1),
+      ButtonViewIconCompact(text: 'Confirm', onPressed: onPressed)
+    ],);
   }
 }
