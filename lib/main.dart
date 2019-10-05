502703fc1f4c2c033afcff09afb588be6f77323a
Sayeed <sayeedhussain19@gmail.com>
2019-10-05 10:10:02 +0530

    [Sayeed] Move launchSetup logic to launch_setup class

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:wastexchange_mobile/app_localizations.dart';
 import 'package:wastexchange_mobile/launch_setup.dart';
-import 'package:wastexchange_mobile/resources/env_repository.dart';
-import 'package:wastexchange_mobile/resources/auth_token_repository.dart';
-import 'package:wastexchange_mobile/resources/key_value_store.dart';
-import 'package:wastexchange_mobile/resources/user_repository.dart';
 import 'package:wastexchange_mobile/routes/router.dart';
 import 'package:wastexchange_mobile/screens/map_screen.dart';
-import 'package:wastexchange_mobile/utils/app_logger.dart';
 import 'package:wastexchange_mobile/utils/app_theme.dart';
 import 'package:wastexchange_mobile/utils/constants.dart';
 
 Future<void> main() async {
-  await LaunchSetup(members: [
-    AppLogger(),
-    EnvRepository(),
-    TokenRepository(),
-    UserRepository(),
-    KeyValueStore()
-  ]).load();
+  await LaunchSetup().load();
   runApp(WasteExchange());
 }
 
