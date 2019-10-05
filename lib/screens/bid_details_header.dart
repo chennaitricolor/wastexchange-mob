6263b69482b3ba7175b25fb5ec567c7ed49b8d20
Sayeed <sayeedhussain19@gmail.com>
2019-10-05 15:47:57 +0530

    [Sayeed] UI Tweaks for bid details header

--- lib/screens/bid_details_header.dart
+++ lib/screens/bid_details_header.dart
@@ -9,7 +9,6 @@ import 'package:wastexchange_mobile/widgets/calendar_card_widget.dart';
 import 'package:wastexchange_mobile/widgets/views/card_view.dart';
 
 class BidDetailsHeader extends StatelessWidget {
-
   const BidDetailsHeader({this.bid, this.user});
 
   final Bid bid;
@@ -23,20 +22,37 @@ class BidDetailsHeader extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: <Widget>[
-          Row(children: <Widget>[
+          Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
             Expanded(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Visibility(visible: !isNullOrEmpty(address), child: Text(address, style: AppTheme.body2)),
-                    Text('${Constants.INR_UNICODE} ${bid.amount}', style: AppTheme.title),
-                    Text(bid.contactName, style: AppTheme.subtitle),
-                  ]),
-            ),
-            const SizedBox(width: 16),
-            BidStatusIconTextView(bidStatus: bid.status)
+                child: Visibility(
+                    visible: !isNullOrEmpty(address),
+                    child: Text(address, style: AppTheme.body2))),
+            const SizedBox(width: 8),
+            Container(
+                child: BidStatusIconTextView(bidStatus: bid.status), width: 100)
           ]),
-          const SizedBox(height: 16),
+          Container(height: 8),
+          Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+            Expanded(
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.person,
+                  size: 18,
+                  color: AppTheme.lightText,
+                ),
+                const SizedBox(width: 5),
+                Flexible(
+                    child: Text(bid.contactName, style: AppTheme.subtitle)),
+              ]),
+            ),
+            const SizedBox(width: 8),
+            Container(
+                alignment: Alignment.centerRight,
+                child: Text('${Constants.INR_UNICODE} ${bid.amount}',
+                    style: AppTheme.title),
+                width: 85)
+          ]),
+          const SizedBox(height: 8),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
