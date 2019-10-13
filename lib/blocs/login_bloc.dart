d70c14dc758889ff2956093163a9b825e4795899
Sayeed <sayeedhussain19@gmail.com>
2019-10-13 13:47:15 +0530

    [Sayeed] Add ToDo

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -8,6 +8,11 @@ import 'package:wastexchange_mobile/utils/constants.dart';
 
 class LoginBloc {
   final UserRepository _userRepository = UserRepository();
+  // TODO(Sayeed): Wrap all streams into a class so that we can do safe adds.
+  /*If a stream is closed and we add to it without checking, the app crashes. It can happen on 
+  a screen which uses stream to listen to api. When a user enters the screen, an api call is triggered but
+  before the response is received, user exits the screen.
+  */
   final StreamController _loginController =
       StreamController<Result<LoginResponse>>();
 
